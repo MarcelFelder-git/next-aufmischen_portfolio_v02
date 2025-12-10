b27fea775aba2297f56c957477598b1358df2396
MarcelFelder-git <marcel.felder.bln@hotmail.com>
2025-12-10 11:25:37 +0100

    locations auth del

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-/* import { handlers } from '@/auth'; // Referring to the auth.ts we just created
-export const { GET, POST } = handlers;
- */
-export function GET() {
-  return NextResponse.json('Funktion löschen, wenn auth benutzt wird');
-}
