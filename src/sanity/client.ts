0cfd03cbbc39747dea3bacd1f925a2bb62b0880f
MarcelFelder-git <marcel.felder.bln@hotmail.com>
2025-12-10 12:41:20 +0100

    next try sanity

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,8 +1,9 @@
 import { createClient } from 'next-sanity';
 
 export const client = createClient({
-	projectId: 'kv534y0p',
-	dataset: 'production',
+	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
 	apiVersion: '2024-01-01',
-	useCdn: true,
+	useCdn: false, // für frische Daten / Drafts
+	//   token: process.env.SANITY_API_READ_TOKEN, // optional
 });
